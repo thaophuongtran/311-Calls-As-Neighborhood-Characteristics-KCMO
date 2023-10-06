39e66cbb4b4bdbd63752c85a8ebc08cae0c9d20c
Thao Tran <ttran@chapinhall.org>
2023-10-05 20:56:06 -0500

    Clean Data

--- data/kcmo2019_2020_datacleaning.R
+++ data/kcmo2019_2020_datacleaning.R
@@ -24,34 +24,16 @@ rm(raw_b)
 rm(raw_c)
 
 #merge
-inc = sec %>% select(NEIGH = NBH_NAME, Income.Level)
-dat = left_join(raw,inc, by="NEIGH")
-dat$Income.Level[dat$NEIGH == "Central Blue Valley and Park Tower Gardens"] = "L"
+inc = sec %>% rename(NEIGH = NBH_NAME)
+raw$NEIGH[raw$NEIGH == "Central Blue Valley and Park Tower Gardens"] = "Central Blue Valley And Park Tower Gardens"
+dat = raw %>% left_join(inc, by="NEIGH")
 
 #drop na neighborhood, income level, source, category, day to close
 dat = dat %>% filter(!(is.na(NEIGH)|is.na(Income.Level)|is.na(SOURCE)|is.na(CATEGORY)|is.na(DAYTOCLOSE)))
 
-#drop sept 2020 for low count
+#drop sept 2020 due to low count
 dat = dat %>% filter(!(CREATEYR==2020 & CREATEMO == 9))
 
-#weight
-dat$weight = 1
-
-#######################################
-# Scraping
-#######################################
-for (i in  1:length(dat$CASEURL)){
-  
-  tryCatch({
-    #scrape description
-    dat$description[i] = strsplit((read_html(dat$CASEURL[i])%>% html_nodes("tr") %>% html_text()),"\r\n")[[10]][3]
-    #remove double spaces
-    dat$description[i] = gsub("  ","",toString(dat$description[i]))
-    
-    print(i)
-  }, error=function(e){"ERROR"})
-}
-
 #save data for further analysis
-save(dat, "data/kcmo2019_2020.rdata")
+save(dat, file = "data/kcmo2019_2020_sec.rdata")
 
